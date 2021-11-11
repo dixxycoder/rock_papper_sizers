69b0c2d0db8b8c2f92983ba3667bde778fa611d1
Grant Spraker <grant.spraker@gmail.com>
2021-11-11 10:44:22 -0700

    got the skeleton of the app.

--- rock_papper_sizers/ViewController.swift
+++ rock_papper_sizers/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var comImage: UIImageView!
+    
+    @IBOutlet weak var playImage: UIImageView!
+    
+    @IBOutlet weak var rockLook: UIImageView!
+    
+    @IBOutlet weak var papperLook: UIImageView!
+    
+    @IBOutlet weak var scissorsLook: UIImageView!
+    
+    var rock = 0
+    var papper = 1
+    var scissors = 2
+ 
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapToWin(_ sender: UITapGestureRecognizer)
+    {
+        playImage.image = UIImage(named: "___")
+    }
+    
 }
 
