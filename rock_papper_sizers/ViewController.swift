7e0590cacacea2d48d97ebd2ec3e249b06440f65
Grant Spraker <grant.spraker@gmail.com>
2021-11-29 11:36:04 -0700

    ALLLL DONNNNNE

--- rock_papper_sizers/ViewController.swift
+++ rock_papper_sizers/ViewController.swift
@@ -13,35 +13,79 @@ class ViewController: UIViewController
     
     @IBOutlet weak var playImage: UIImageView!
     
-    @IBOutlet weak var rockLook: UIImageView!
+    //has rock, paper, and scissors on it
+    @IBOutlet weak var picesOfGame: UIStackView!
     
-    @IBOutlet weak var papperLook: UIImageView!
+    @IBOutlet var picesGame: [UIImageView]!
+
+    @IBOutlet weak var winOrLoss: UILabel!
     
-    @IBOutlet weak var scissorsLook: UIImageView!
-    
-//     rock = 0
-//     papper = 1
-//     scissors = 2
-    
-    let randomNumber = Int.random(in: 0...2)
+    var images:[UIImage] = []
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        rockLook.image = UIImage(named: "0")
-        papperLook.image = UIImage(named: "1")
-        scissorsLook.image = UIImage(named: "2")
-        
-        comImage.image = UIImage(named: "\(randomNumber)")
-
-    }
-
-    @IBAction func tapToWin(_ sender: UITapGestureRecognizer)
-    {
-        
-        playImage.image = UIImage(named: "\(randomNumber)")
     }
     
-}
+    var playerSelect = 5
+    
+    @IBAction func tapToWin(_ sender: UITapGestureRecognizer)
+    {
+    
+        
+        let rock = UIImage(named: "rock")
+        let paper = UIImage(named: "paper")
+        let scissors = UIImage(named: "scissors")
+        
+        images = [rock, paper, scissors] as! [UIImage]
+        
+        let randomNumber = Int.random(in: 0...2)
+        var comSelect = images[randomNumber]
+        comImage.image = comSelect
+        
+        let selectedPoint = sender.location(in: picesOfGame)
+        
+        for label in picesGame
+    {
+            if
+                label.frame.contains(selectedPoint)
+            {
+                playerSelect = label.tag
+                
+                playImage.image = images[playerSelect]
+                    
+                    print(playerSelect)
+            }
+        
+            
+    }
+        
+        
+        if playerSelect == randomNumber
+        {
+            winOrLoss.text = "Tie"
 
+        }else if playerSelect == 2 && randomNumber == 0
+        {
+            winOrLoss.text = "loss"
+
+        }else if playerSelect == 1 && randomNumber == 2
+        {
+            winOrLoss.text = "loss"
+
+        }else if playerSelect == 0 && randomNumber == 1
+        {
+            winOrLoss.text = "loss"
+
+        }else if playerSelect == 0 && randomNumber == 2
+        {
+            winOrLoss.text = "Win"
+        }else if playerSelect == 1 && randomNumber == 0
+        {
+            winOrLoss.text = "Win"
+        }else if playerSelect == 2 && randomNumber == 1
+        {
+            winOrLoss.text = "Win"
+        }
+    }
+}
